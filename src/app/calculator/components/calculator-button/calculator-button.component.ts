1aa92cd54d86e9dd3e577145302c3b68680ef97e
Sergio <sgomez@movisat.com>
2025-02-06 22:57:02 +0100

    Refactor calculator button component: add CSS styles, update input handling, and fix template binding

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,23 +1,34 @@
-import { ChangeDetectionStrategy, Component, Host, HostBinding, input, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  input,
+} from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
   standalone: true,
+  imports: [],
   templateUrl: './calculator-button.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: './calculator-button.component.css',
   host: {
-    'class': 'w-1/4 border-r border-b border-indigo-400',
-  }
+    class: 'w-1/4 border-r border-b border-indigo-400',
+  },
 })
-export class CalculatorButtonComponent  {
 
-  isComand = input(false, {
-  transform: (value: boolean | string) => 
-    typeof value === 'string' ? value === 'true' : value
+export class CalculatorButtonComponent {
+  public isCommand = input(false, {
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value,
   });
 
-  @HostBinding('class.bg-indigo-700') get class() {
-    return this.isComand();
-  }
+  public isDoubleSize = input(false, {
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value,
+  });
 
+  @HostBinding('class.is-command') get commandStyle() {
+    return this.isCommand();
+  }
 }
