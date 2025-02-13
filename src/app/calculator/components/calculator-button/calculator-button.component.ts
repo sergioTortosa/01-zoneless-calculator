8a415b6bbb99564b89c0c809bf25339ceaca6602
sgomez <sgomez@movisat.com>
2025-02-13 08:25:38 +0100

    seccion 3.

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,9 +1,13 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   HostBinding,
   input,
   OnInit,
+  output,
+  signal,
+  viewChild,
   ViewEncapsulation,
 } from '@angular/core';
 
@@ -15,13 +19,20 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './calculator-button.component.css',
   host: {
-    class: 'w-1/4 border-r border-b border-indigo-400',
+    class: 'border-r border-b border-indigo-400',
+    '[class.w-2/4]': 'isDoubleSize()',
+    '[class.w-1/4]': '!isDoubleSize()',
     // attribute: 'hola',
     // 'data-size': 'XL',
   },
   // encapsulation: ViewEncapsulation.None,
 })
 export class CalculatorButtonComponent {
+  public isPressed = signal(false);
+
+  public onClick = output<string>();
+  public contentValue = viewChild<ElementRef<HTMLButtonElement>>('button');
+
   public isCommand = input(false, {
     transform: (value: boolean | string) =>
       typeof value === 'string' ? value === '' : value,
@@ -36,7 +47,31 @@ export class CalculatorButtonComponent {
   //   return this.isCommand();
   // }
 
-  @HostBinding('class.w-2/4') get commandStyle() {
-    return this.isDoubleSize();
+  // @HostBinding('class.w-2/4') get commandStyle() {
+  //   return this.isDoubleSize();
+  // }
+
+  handleClick() {
+    if (!this.contentValue()?.nativeElement) {
+      return;
+    }
+
+    const value = this.contentValue()!.nativeElement.innerText;
+
+    this.onClick.emit(value.trim());
+  }
+
+  public keyboardPressedStyle(key: string) {
+    if (!this.contentValue()) return;
+
+    const value = this.contentValue()!.nativeElement.innerText;
+
+    if (value !== key) return;
+
+    this.isPressed.set(true);
+
+    setTimeout(() => {
+      this.isPressed.set(false);
+    }, 100);
   }
 }
